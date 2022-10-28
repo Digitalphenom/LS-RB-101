d3703bdfbcd05a643f230466ab9677892f56cf04
Sedrick Penate <digitalphenom0@gmail.com>
2022-10-28 15:59:38 -0700

    rubocop refactor

--- lesson_02/calculator.rb
+++ lesson_02/calculator.rb
@@ -11,7 +11,7 @@ def prompt(message)
 end
 
 def valid_number?(num)
-  num.to_i() !=0
+  num.to_i() != 0
 end
 
 prompt("Welcome to Calculator!")
@@ -19,7 +19,7 @@ name = prompt("Enter your name")
 name = Kernel.gets().chomp()
 prompt("Welcome #{name}")
 
-loop do  
+loop do
   if name.empty?()
     prompt("Make sure to use a valid name.")
   else
@@ -28,85 +28,69 @@ loop do
 end
 
 loop do
-
   number1 = 0
   loop do
     prompt("Pick your first number")
     number1 = Kernel.gets().chomp()
-    
     if valid_number?(number1)
       break
     else
       prompt("are you sure you entered a valid number?")
-   end
+    end
   end
-  
   number2 = 0
   loop do
     prompt("Pick your second number")
-    number2  = Kernel.gets().chomp()
-    
+    number2 = Kernel.gets().chomp()
     if valid_number?(number2)
       break
     else
       prompt("are you sure you entered a valid number?")
     end
   end
-  
-  
   operator_prompt = <<-MSG
     What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
+    1)add
+    2)subtract
+    3)multiply
+    4)divide
   MSG
-  
   prompt(operator_prompt)
-
   operator = 0
   loop do
     operator = Kernel.gets().chomp()
-
     if %w(1 2 3 4).include?(operator)
       break
     else
       prompt("Must choose 1,2,3 or 4")
+    end
   end
-end
-
-def operation_to_message(op)
-  case op
-  when "1"
-    "Adding"
-  when "2"
-    "Subtracting"
-  when "3"
-    "Multiplying"
-  when "4"
-    "Dividing"
-  end
-end
-
-prompt("#{operation_to_message(operator)} the two numbers....")
-
-  result = case operator
-  when "1"
-    result = number1.to_i() + number2.to_i()
-  when "2"
-    result = number1.to_i() - number2.to_i()
-  when "3"
-    result = number1.to_i() * number2.to_i()
-  else
-    result = number1.to_f() / number2.to_f()  
+  def operation_to_message(op)
+    case op
+    when "1"
+      "Adding"
+    when "2"
+      "Subtracting"
+    when "3"
+      "Multiplying"
+    when "4"
+      "Dividing"
+    end
   end
 
+  prompt("#{operation_to_message(operator)}the two numbers..")
+    case operator
+    when "1"
+      result = number1.to_i() + number2.to_i()
+    when "2"
+      result = number1.to_i() - number2.to_i()
+    when "3"
+      result = number1.to_i() * number2.to_i()
+    when "4"
+      result = number1.to_f() / number2.to_f()
+    end
   prompt("The result is #{result}")
-  
   prompt("Do you want to perform another calculation? (Y to calculate again)")
-
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?("y")
 end
-
-  
