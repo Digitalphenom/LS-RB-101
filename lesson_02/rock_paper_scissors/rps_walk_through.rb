3ef3bfd781f316a4b95e45a88e5227de629d20ed
Sedrick Penate <digitalphenom0@gmail.com>
2022-11-03 12:32:15 -0700

    rubocop line spacing fix

--- lesson_02/rock_paper_scissors/rps_walk_through.rb
+++ lesson_02/rock_paper_scissors/rps_walk_through.rb
@@ -10,9 +10,9 @@ def display_results(choice, computer_choice)
     (choice == "scissors" && computer_choice == "paper")
     prompt("You WIN!!")
   elsif choice == "scissors" && computer_choice == "rock" ||
-        (choice == "rock" && computer_choice == "paper") ||
-        (choice == "paper" && computer_choice == "scissors")
-        prompt("You Lose..")
+    (choice == "rock" && computer_choice == "paper") ||
+    (choice == "paper" && computer_choice == "scissors")
+    prompt("You Lose..")
   else
     prompt("TIE try again")
   end
@@ -20,28 +20,21 @@ end
 
 choice = ""
 loop do
-loop do
-  prompt("Choose one: rock, paper, scissors")
-  choice = Kernel.gets().chomp()
-  
-  if VALID_CHOICES.include?(choice)
-    break
-  else
-    prompt("Thats not a valid choice.")
+  loop do
+    prompt("Choose one: rock, paper, scissors")
+    choice = Kernel.gets().chomp()
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("Thats not a valid choice.")
+    end
   end
-end
   computer_choice = VALID_CHOICES.sample
-
-  display_results(choice, computer_choice)
-
   Kernel.puts("You chose: #{choice} computer chose: #{computer_choice}")
-
+  puts display_results(choice, computer_choice)
   prompt("Do you want to play again??")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?("y")
 end
 
 prompt("Thank you for playing!")
-    
-  
-
