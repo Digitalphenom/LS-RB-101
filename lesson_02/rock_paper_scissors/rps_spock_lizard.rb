51325b860ecfd22b61afb61cbb7013604dd54a9b
Sedrick Penate <digitalphenom0@gmail.com>
2022-11-05 13:06:37 -0700

    changed player choice into hash

--- lesson_02/rock_paper_scissors/rps_spock_lizard.rb
+++ lesson_02/rock_paper_scissors/rps_spock_lizard.rb
@@ -4,28 +4,16 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def name_change(choice)
-  case choice
-  when "r"
-    choice = "Rock"
-  when "p"
-    choice = "Paper"
-  when "s"
-    choice = "Scissors"
-  when "sp"
-    choice = "Spock"
-  when "l"
-    choice = "Lizard"
-  end
-end
+CHOICE = { "p" => :Paper, "r" => :Rock,
+        "s" => :Scissors, "sp" => :Spock, "l" => :Lizard }
 
 def win?(first, second)
-   first == "r" && (second == "l" || second == "s") ||
-   first == "p" && (second == "sp" || second == "r") ||
-   first == "s" && (second == "l" || second == "p") ||
-   first == "sp" && (second == "r" || second == "s") ||
-   first == "l" && (second == "p" || second == "sp")
-  end
+  first == "r" && (second == "l" || second == "s") ||
+    first == "p" && (second == "sp" || second == "r") ||
+    first == "s" && (second == "l" || second == "p") ||
+    first == "sp" && (second == "r" || second == "s") ||
+    first == "l" && (second == "p" || second == "sp")
+end
 
 def display_results(first, second)
   if win?(first, second)
@@ -38,33 +26,27 @@ def display_results(first, second)
 end
 
 choice = ""
-user_count = 0
-computer_count = 0
-def keep_count(user, computer)
-  
-end
+
 loop do
   loop do
-    operator_prompt = <<~EOM
+    operator_prompt = <<~MSG
     What do you choose?
-      R-Rock | P-Paper | S-Scisorrs | SP-Spock | L-Lizard
-      EOM
-      prompt(operator_prompt)
-      #prompt("Choose one: rock, paper, scissors")
-      choice = Kernel.gets().chomp()
-      if VALID_CHOICES.include?(choice)
-        break
-      else
-        prompt("Thats not a valid choice.")
-      end
+    R-Rock | P-Paper | S-Scisorrs | SP-Spock | L-Lizard
+    MSG
+    prompt(operator_prompt)
+    choice = Kernel.gets().chomp()
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("Thats not a valid choice.")
     end
-    computer_choice = VALID_CHOICES.sample
+  end
+  computer_choice = VALID_CHOICES.sample
+  Kernel.puts("You chose: #{CHOICE[choice]} computer chose:#{CHOICE[choice]}")
 
-    Kernel.puts("You chose: #{name_change(choice)} computer chose: #{name_change(computer_choice)}")
   puts display_results(choice, computer_choice)
   prompt("Do you want to play again??")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?("y")
 end
-
 prompt("Thank you for playing!")
