9de5c9791432898589d57ff52c6a550fb1ffbb31
Sedrick Penate <digitalphenom0@gmail.com>
2022-11-18 00:41:19 -0800

    refactored edge cases

--- small_problems/easy_02/greeting_user.rb
+++ small_problems/easy_02/greeting_user.rb
@@ -1,9 +1,18 @@
 
   print "What is your name? "
   name = gets.chomp
-  if name.include?("!")
+  #if name.include?("!")
+  #  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?! "
+  #else
+  #  puts "Hello #{name.capitalize}."
+  #end
+  if name[-1] == "!"
+    name = name.chop
     puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?! "
   else
     puts "Hello #{name.capitalize}."
   end
-  
+
+  # this solution works but does not consider edge cases.
+  # s!ed, !
+  #
