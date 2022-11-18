ec7a8c1b7be4292ac9f23a31416e172a370c1456
Sedrick Penate <digitalphenom0@gmail.com>
2022-11-17 16:09:20 -0800

    refactored collections and final num test

--- small_problems/easy_03/searching_101.rb
+++ small_problems/easy_03/searching_101.rb
@@ -2,29 +2,18 @@ def prompt(enter)
   Kernel.puts("=>#{enter}")
 end
 total = []
-num_collection = {
-  :number_one => "1st",
-  :number_two => "2nd",
-  :number_three => "3rd",
-  :number_four => "4th",
-  :number_five => "5th"
-}
+num_collection = ["1st","2nd","3rd","4th","5th"]
 
-num_collection.each do |_, val|
-  prompt("Enter the #{val} number:")
+num_collection.each do |num|
+  prompt("Enter the #{num} number:")
   numbers = gets.chomp.to_i
   total << numbers
 end
 prompt("Enter the last number:")
 final_num = gets.chomp.to_i
 
-total.each do |num|
-  if final_num == num
-    prompt("The number #{final_num} appears in #{total}.")
-    break
-  else
-    prompt("The number #{final_num} is not included in #{total}.")
-    break
-  end
+if total.include?(final_num)
+  prompt("The number #{final_num} appears in #{total}.")
+else
+  prompt("The number #{final_num} is not included in #{total}.")
 end
-
