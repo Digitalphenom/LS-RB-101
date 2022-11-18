6d8d73e3d768b13bf76d218e711c397d7d222544
Sedrick Penate <digitalphenom0@gmail.com>
2022-11-18 00:34:00 -0800

    add_greeting_user complete

---
+++ small_problems/easy_02/greeting_user.rb
@@ -0,0 +1,9 @@
+
+  print "What is your name? "
+  name = gets.chomp
+  if name.include?("!")
+    puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?! "
+  else
+    puts "Hello #{name.capitalize}."
+  end
+  
