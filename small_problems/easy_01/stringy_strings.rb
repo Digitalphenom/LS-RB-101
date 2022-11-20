e1034e11edb0571a498ecb8a47a08b5a40d93066
Sedrick Penate <digitalphenom0@gmail.com>
2022-11-19 22:08:03 -0800

    further explore complete

--- small_problems/easy_01/stringy_strings.rb
+++ small_problems/easy_01/stringy_strings.rb
@@ -27,3 +27,34 @@ puts stringy(7) == '1010101'
 # A loop can be used to iterate through the length of the argument number.
 # use a count variable to punch in 1's and 0's into a collection based on whether the value in the iteration is even or odd.
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+puts 
+
+# given a positive integer and an optional argument set to 1
+# return a string of alternating 1's & 0's 
+# return a string of alternating 0's & 1's when the optional argument is set to 0
+
+
+def stringy_two(num, order= 1)
+  collection = []
+  count = 0
+  loop do
+    if order == 1 && count % 2 == 0
+      collection << '1'
+    elsif order == 1 && count % 2 == 1
+      collection << '0' 
+    elsif order == 0 && count % 2 == 0
+      collection << '0' 
+    elsif order == 0 && count % 2 == 1
+      collection << '1'
+    end
+  count += 1
+  break if count == num
+  end
+  collection.join
+end
+
+puts stringy_two(6,0) == '101010'
+puts stringy_two(9,0) == '101010101'
+puts stringy_two(4,1) == '1010'
+puts stringy_two(7,1) == '1010101'
