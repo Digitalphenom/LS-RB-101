a18e3c74f66200df1519e001d7e8d1fe3ea5034c
Sedrick Penate <digitalphenom0@gmail.com>
2022-11-27 15:10:46 -0800

    re-did code break down

--- lesson_05/take_apart_05.rb
+++ lesson_05/take_apart_05.rb
@@ -4,9 +4,13 @@
   end
 end
 
-# line 1 is a method call on the object outer array. it does not have any side-effects nor a return value.
+# we have a nested array with 2 method calls meant to access the sub-arrays elements and transform/modify by * 2.
 
-# we have a nested loop meant to access the sub-arrays elements and transform/modify by * 2.
-# The second method call to arr return value is => [[2,4],[6,8]]
+# line 1 is a method call on the object outer-array. its return value is a new array of the object it was called on without transformation.
+# line 2 is a method call (map) on line 1's argument |arr|.
+# it has its own argument |num| to access the inner values of the nested array. its return value is its blocks return value on line 3
+# line 3 performs transformation on the inner elements |num| * 2
+# the return value of the second method call is a new tranformed array => [2,4][6,8]
+# the return value of the first method call is is a new tranformed array => [[2,4],[6,8]]
 
 
