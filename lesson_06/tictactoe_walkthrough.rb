a93131387460c5a83e5be3a254c2a0c598f67a8d
Sedrick Penate <digitalphenom0@gmail.com>
2022-11-29 20:10:59 -0800

    formatting refactor

--- lesson_06/tictactoe_walkthrough.rb
+++ lesson_06/tictactoe_walkthrough.rb
@@ -7,6 +7,7 @@ COMPUTER_MARKER = "O"
 def prompt(str)
   puts "=> #{str}"
 end
+
 def display_board(brd)
   system "clear"
   puts "You're #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
@@ -27,12 +28,12 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
@@ -41,10 +42,10 @@ def player_places_piece!(brd)
     prompt "Choose a square (#{empty_squares(brd).join(", ")}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-      prompt "Sorry, that's not a valid choice."
+    prompt "Sorry, that's not a valid choice."
   end
 
-    brd[square] = PLAYER_MARKER
+  brd[square] = PLAYER_MARKER
 end
 
 def computer_places_piece!(brd)
@@ -61,18 +62,18 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1,2,3],[4,5,6],[7,8,9]] + # rows
-                  [[1,4,7],[2,5,8],[3,6,9]] + # colums
-                  [[1,5,9],[3,5,7]]
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # colums
+                  [[1, 5, 9], [3, 5, 7]]
 
   winning_lines.each do |line|
     if brd[line[0]] == PLAYER_MARKER &&
-      brd[line[1]] == PLAYER_MARKER &&
-      brd[line[2]] == PLAYER_MARKER
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
       return "Player"
     elsif brd[line[0]] == COMPUTER_MARKER &&
-      brd[line[1]] == COMPUTER_MARKER &&
-      brd[line[2]] == COMPUTER_MARKER
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
       return "Computer"
     end
   end
@@ -80,33 +81,27 @@ def detect_winner(brd)
 end
 
 loop do
-  
-  
   board = initialize_board
-  
+
   loop do
     display_board(board)
-    
+
     player_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
-    
+
     computer_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
   end
-  
+
   if someone_won?(board)
     prompt "#{detect_winner(board)} won!"
   else
     prompt "It's a tie!"
   end
-  
+
   prompt "Play again? (y or no)"
   answer = gets.chomp
   break unless answer.downcase.start_with?("y")
 end
 
 prompt "Thanks for playing TicTacToe Goodbye!"
-
-
-
-
