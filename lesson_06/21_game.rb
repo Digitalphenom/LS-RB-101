2a535645a40037c502375d7f32abafba3af24f3e
Sedrick Penate <digitalphenom0@gmail.com>
2022-12-10 11:28:00 -0800

    updated initial values

--- lesson_06/21_game.rb
+++ lesson_06/21_game.rb
@@ -1,7 +1,25 @@
-CARD_DECK = {
-  :card: => [2, 3, 4, 5, 6, 7, 8, 9, 10],
-  [:jack, :queen, :king] => 10,
-  :ace => [1, 11]
-}
+require "yaml"
+require "pry"
 
-CARD_DECK.values[1]
+MESSAGES = YAML.load_file('messages.yml')
+
+INITIAL_MARKER = " "
+
+VALUES = ('2'..'10').to_a
+
+SUITS = {
+  'D' => '♦',
+  'S' => '♠',
+  'H' => '♥',
+  'C' => '♣',
+  'Q' => '?'
+        }
+
+  FACES = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', ' ','A']
+
+player_total = 0
+dealer_total = 0
+
+  def prompt(str)
+    puts "=> #{str}"
+  end
