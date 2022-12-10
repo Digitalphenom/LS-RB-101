14d434a6a65df0c79b9da6f772960dcc1185e72d
Sedrick Penate <digitalphenom0@gmail.com>
2022-12-10 12:03:35 -0800

    set display_screen & initial card display value

--- lesson_06/21_game.rb
+++ lesson_06/21_game.rb
@@ -24,12 +24,23 @@ dealer_total = 0
     puts "=> #{str}"
   end
 
+  def initialize_suit_values
+    blank_suits = {}
+    (1..4).each { |num| blank_suits[num] = INITIAL_MARKER }
+    blank_suits
+  end
 
-def display_deck(suits, crd)
+  def initialize_face_values
+    blank_face = {}
+    (1..4).each { |num| blank_face[num] = INITIAL_MARKER }
+    blank_face
+  end
+
+def display_deck(suits, face)
   system "clear"
   prompt "You're Bottom. Dealer is Top"
   puts "      ____________         ______________"
-  puts "     |          #{crd}  |       |           #{crd}  |"
+  puts "     |          #{face[1]}  |       |           #{face[2]}  |"
   puts "     |            |       |             |    "
   puts "     |            |       |             |"
   puts "     |      #{suits[1]}     |       |       #{suits[2]}     |"
@@ -38,7 +49,7 @@ def display_deck(suits, crd)
   puts "     |____________|       |_____________|"
   puts "                                    "
   puts "     ____________         ______________"
-  puts "    |          #{crd}  |       |           #{crd}  |"
+  puts "    |          #{face[3]}  |       |           #{face[4]}  |"
   puts "    |            |       |             |    "
   puts "    |            |       |             |"
   puts "    |      #{suits[3]}     |       |      #{suits[4]}      |"
@@ -47,3 +58,15 @@ def display_deck(suits, crd)
   puts "    |____________|       |_____________|"
   puts "                                    "
 end
+
+def display_screen(dealer, player, suit, face)
+  prompt "Welcome to the game 21!"
+  prompt "Ready? Press (y) to Start"
+  start = gets.chomp
+  display_deck(suit, face) if start.include?("y")
+  # prompt " Dealer #{dealer}          Player #{player}"
+end
+
+blank_suit = initialize_suit_values()
+blank_face = initialize_face_values()
+display_screen(player_total, dealer_total, blank_suit, blank_face)
