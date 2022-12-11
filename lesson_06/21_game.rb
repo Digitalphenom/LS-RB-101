1157890e5fdf85615e98cf91b493bc0b5e4c75e1
Sedrick Penate <digitalphenom0@gmail.com>
2022-12-11 14:12:54 -0800

    updated method names set card values & convert values method

--- lesson_06/21_game.rb
+++ lesson_06/21_game.rb
@@ -12,44 +12,46 @@ SUITS = {
   'S' => '♠',
   'H' => '♥',
   'C' => '♣',
-  'Q' => '?'
         }
 
-  FACES = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', ' ','A']
+  FACES = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A',]
 
-player_total = 0
-dealer_total = 0
+player_stash = []
+dealer_stash = []
+
+player_total = []
+dealer_total = []
 
   def prompt(str)
     puts "=> #{str}"
   end
 
-  def initialize_suit_values
-    blank_suits = {}
-    (1..4).each { |num| blank_suits[num] = INITIAL_MARKER }
-    blank_suits
+  def generate_suit_values
+    suit_value = {}
+    (1..4).each { |num| suit_value[num] = SUITS.values.sample }
+    suit_value
   end
 
-  def initialize_face_values
-    blank_face = {}
-    (1..4).each { |num| blank_face[num] = INITIAL_MARKER }
-    blank_face
+  def generate_face_values
+    face_value = {}
+    (1..4).each { |num| face_value[num] = FACES.sample }
+    face_value
   end
 
 def display_deck(suits, face)
   system "clear"
   prompt "You're Bottom. Dealer is Top"
   puts "      ____________         ______________"
-  puts "     |          #{face[1]}  |       |           #{face[2]}  |"
+  puts "     |         #{face[1]}  |       |             |"
   puts "     |            |       |             |    "
   puts "     |            |       |             |"
-  puts "     |      #{suits[1]}     |       |       #{suits[2]}     |"
+  puts "     |      #{suits[1]}     |       |       #{"?"}     |"
   puts "     |            |       |             |"
   puts "     |            |       |             |"
   puts "     |____________|       |_____________|"
   puts "                                    "
   puts "     ____________         ______________"
-  puts "    |          #{face[3]}  |       |           #{face[4]}  |"
+  puts "    |         #{face[3]}  |       |          #{face[4]}  |"
   puts "    |            |       |             |    "
   puts "    |            |       |             |"
   puts "    |      #{suits[3]}     |       |      #{suits[4]}      |"
@@ -64,9 +66,66 @@ def display_screen(dealer, player, suit, face)
   prompt "Ready? Press (y) to Start"
   start = gets.chomp
   display_deck(suit, face) if start.include?("y")
-  # prompt " Dealer #{dealer}          Player #{player}"
+  prompt " Dealer #{dealer.sum}          Player #{player.sum}"
 end
 
-blank_suit = initialize_suit_values()
-blank_face = initialize_face_values()
-display_screen(player_total, dealer_total, blank_suit, blank_face)
+def store_card_values(face_value, dealer_stash, player_stash)
+
+  player_stash << face_value.values_at(3, 4)
+  dealer_stash << face_value.values_at(1, 2)
+end
+
+def player_turn()
+  prompt "Player Turn"
+  prompt "ENTER 1) HIT   |   2) STAY"
+  answer = gets.chomp
+  
+  if answer.include?("1")
+    generate_cards()
+  elsif answer.include?("2")
+    dealer_turn()
+  end
+end
+
+def convert_values(p_total, d_total, player_stash, dealer_stash)
+
+  converted_player_values = player_stash.map do |deck|
+    deck.map do |value|
+      deck = value.to_i 
+      if deck.zero?
+        deck = 10
+      else
+        deck
+      end
+    end
+  end
+
+  converted_dealer_values = dealer_stash.map do |deck|
+    deck.map do |value|
+      deck = value.to_i 
+      if deck.zero?
+        deck = 10
+      else
+        deck
+      end
+    end
+  end
+end
+
+
+
+
+
+# Start Game
+loop do
+  suit_value = generate_suit_values()
+  face_value = generate_face_values()
+
+  display_screen(player_total, dealer_total, suit_value, face_value)
+  store_card_values(face_value, dealer_stash, player_stash)
+  binding.pry
+  convert_values(player_total, dealer_total, dealer_stash, player_stash)
+  player_turn()
+  
+  break
+end
