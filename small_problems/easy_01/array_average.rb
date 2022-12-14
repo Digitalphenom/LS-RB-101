3a8485351c131c66f315488ce4485755104630c1
Sedrick Penate <digitalphenom0@gmail.com>
2022-12-14 13:58:10 -0800

    array_average complete

---
+++ small_problems/easy_01/array_average.rb
@@ -0,0 +1,11 @@
+
+def average(num)
+  num.sum / num.length
+end
+
+puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
+
+# to get a mean value, add up all values and divide sum
+#  by total number of values.
