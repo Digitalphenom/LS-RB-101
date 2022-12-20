f67dc7b7196045b8afae373333352eabea8fa662
Sedrick Penate <digitalphenom0@gmail.com>
2022-12-20 12:41:00 -0800

    additional answer

--- small_problems/easy_03/searching_101.rb
+++ small_problems/easy_03/searching_101.rb
@@ -17,3 +17,34 @@ if total.include?(final_num)
 else
   prompt("The number #{final_num} is not included in #{total}.")
 end
+
+#---------------------------
+# 12/20/22 alternate approach
+
+def assign_ordinal(num)
+  num = num.to_s
+  case num
+  when "1" ; num << "st"
+  when "2" ; num << "nd"
+  when "3" ; num << "rd"
+  else     ; num << "th"
+  end
+end
+
+num = 0 
+arr = []
+
+until arr.count == 5
+  num += 1
+  puts "Enter the #{assign_ordinal(num)} number:"
+  answer = gets.chomp
+  arr << answer
+end
+
+puts "Enter the last number:"
+last_num = gets.chomp
+
+sixth_num = arr.include?(last_num) ? last_num << " appears" : last_num << " does not appear"
+arr = arr.map { |num| num.to_i }
+
+puts "The number #{sixth_num} in #{arr}"
