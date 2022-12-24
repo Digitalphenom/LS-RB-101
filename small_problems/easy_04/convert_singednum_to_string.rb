93af26bbb4a673744b61a8ff93e78e6b2b2b9404
Sedrick Penate <digitalphenom0@gmail.com>
2022-12-23 16:34:18 -0800

    refactored

--- small_problems/easy_04/convert_singednum_to_string.rb
+++ small_problems/easy_04/convert_singednum_to_string.rb
@@ -1,26 +1,25 @@
 
 def signed_integer_to_string(num)
   collection = []
-  if num.negative?
-    num -= (num + num) 
-    collection << "-"
-  else
-    collection << "+"
-  end
-  
+  return "0" if num == 0
+
+  num.negative? ? collection << "-" : collection << "+"
+  num -= (num + num) if collection.include?("-")
+
   num = num.digits.reverse
   number_map = {
     0 => "0", 1 => "1", 2 => "2",
     3 => "3", 4 => "4", 5 => "5",
     6 => "6", 7 => "7", 8 => "8",
     9 => "9"
-}
+                }
+
   num.map do |element|
   collection <<  number_map[element] if element
   end
+
   string = collection.join
-  string.slice!(0) if string == "+0"
-  p string
+  string
 end
 
 signed_integer_to_string(4321) == '+4321'
