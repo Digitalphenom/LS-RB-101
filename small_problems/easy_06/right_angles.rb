bbcc7e3490b5d0aaf94c84a45c77320a496c1a6d
Sedrick Penate <digitalphenom0@gmail.com>
2022-12-29 19:33:48 -0800

    need to combine these loops somehow

--- small_problems/easy_06/right_angles.rb
+++ small_problems/easy_06/right_angles.rb
@@ -1,16 +1,25 @@
 require 'pry'
 def triangle(digit)
-  total = digit
-  spaces = digit
-  #digit = 0
-
-  until digit == 0
-    digit.times do |i|
-      e = print "."
+  space_count = digit
+  start_count = 0
+  loop do
+    space_count.times do |i|
+      print "."
     end
     puts "\n"
-    digit -= 1
+    space_count -= 1
+    break if space_count == 1 
   end
+  loop do
+    start_count += 1
+    start_count.times do |i|
+      print "*"
+    end
+    puts "\n"
+    break if start_count == digit
+  end
+
 end
 
-triangle(4)
+triangle(5)
+
