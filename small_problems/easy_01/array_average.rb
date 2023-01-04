1c0fda9c87ace0ca102a53ec85206750f42f65cd
Sedrick Penate <digitalphenom0@gmail.com>
2023-01-04 15:48:55 -0800

    updated answer with a loop

--- small_problems/easy_01/array_average.rb
+++ small_problems/easy_01/array_average.rb
@@ -1,7 +1,14 @@
 
 def average(num)
   # if desired output is a float => num = num.map {|x| x.to_f}
-  p num.sum / num.length
+  sum = 0
+  count = 0
+  loop do
+    sum += num[count]
+    count += 1
+    break if count == num.size
+  end
+  sum / num.size
 end
 
 puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
@@ -10,9 +17,6 @@ puts average([9, 47, 23, 95, 16, 52]) == 40
 
 # to get a mean value, add up all values and divide sum
 #  by total number of values.
-
-
-# LS solution
 
 def average(numbers)
   sum = numbers.reduce { |sum, number| sum + number }
