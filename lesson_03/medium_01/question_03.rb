ac9419897a799aea6952ff789d1e614bf51dfb68
Sedrick Penate <digitalphenom0@gmail.com>
2023-02-11 14:48:34 -0800

    updated answer

--- lesson_03/medium_01/question_03.rb
+++ lesson_03/medium_01/question_03.rb
@@ -4,10 +4,10 @@ def factors(number)
   if number == 0 || number.negative?
     return "Please enter a valid character"
   end
-  begin
+  until divisor == 0
     factors << number / divisor if number % divisor == 0
     divisor -= 1
-  end until divisor == 0
+  end 
   factors
 end
 # the code below accomplishes this more succintly.
