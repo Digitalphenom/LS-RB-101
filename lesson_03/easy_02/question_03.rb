ecf99d10ecccd91ca432e470f0795a6a2dc95379
Sedrick Penate <digitalphenom0@gmail.com>
2023-02-10 16:42:55 -0800

    updated answer

--- lesson_03/easy_02/question_03.rb
+++ lesson_03/easy_02/question_03.rb
@@ -1,10 +1,8 @@
-ages = { 
-  "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
-additional_ages = {
-  "Marilyn" => 22, "Spot" => 237 }
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+additional_ages = { "Marilyn" => 22, "Spot" => 237 }
 
-ages additional_ages
 
-#ages["Marilyn"] = 25
-#ages["Spot"] = 2
+ages.merge!(additional_ages)
+additional_ages.each { |key, val| ages[key] = val }
+additional_ages.each { |key, val| ages.store(key,val) }
 p ages
