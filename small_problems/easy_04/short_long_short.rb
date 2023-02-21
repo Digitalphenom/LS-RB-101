0dbfaacc441c543b95e7250b082ba818516b45fc
Sedrick Penate <digitalphenom0@gmail.com>
2023-02-20 19:07:12 -0800

    refactored additional answer

--- small_problems/easy_04/short_long_short.rb
+++ small_problems/easy_04/short_long_short.rb
@@ -26,3 +26,26 @@ end
 p short_long_short('abc', 'defgh') == "abcdefghabc"
 p short_long_short('abcde', 'fgh') == "fghabcdefgh"
 p short_long_short('', 'xyz') == "xyz"
+
+# 2/20/23 additional answer
+
+
+def short_long_short(str1, str2)
+  if str1.size < str2.size
+    short, long = str1, str2
+  else
+    short, long = str2, str1
+  end
+  puts "#{short}#{long}#{short}"
+end
+
+# can be shortened to:
+def short_long_short(str1, str2)
+  short, long = str1.size < str2.size ? [str1, str2] : [str2, str1]
+  puts "#{short}#{long}#{short}"
+end
+
+
+short_long_short("abc", "defgh")
+short_long_short("abcde", "fgh")
+short_long_short("", "xyz")
