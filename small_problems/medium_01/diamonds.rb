946f6837127b11d93674b0532f32f4e1e58065d6
Sedrick Penate <digitalphenom0@gmail.com>
2023-03-09 18:25:34 -0800

    further exploration

--- small_problems/medium_01/diamonds.rb
+++ small_problems/medium_01/diamonds.rb
@@ -2,18 +2,17 @@ def diamond(digit)
   space = Hash.new
   diamond = Hash.new
   arr = (1..digit).to_a
-
   arr.select! { |num| num % 2 == 1 }
 
   arr.each { |num| diamond[num] = "*" * num }
   arr.reverse_each.with_index { |num, i| space[num] = " " * i }
 
-# further exploration
-#  diamond.each do |k,v|
-#   next if k == 1 || k.even?
-#   v[1..-2] = " " * k
-# end
-# space[1] << " "
+  # further exploration
+  #  diamond.each do |k,v|
+  #   next if k == 1 || k.even?
+  #   v[1..-2] = " " * k
+  # end
+  # space[1] << " "
 
   arr.each { |num| puts "#{space[num]}#{diamond[num]}#{space[num]}"}
   arr.reverse_each do |num|
@@ -21,8 +20,6 @@ def diamond(digit)
     puts "#{space[num]}#{diamond[num]}#{space[num]}"
   end
 end
-
-
 
  diamond(9)
 
