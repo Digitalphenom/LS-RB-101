153488e3fcc3b1d145659f181c724c1c64d9cfa7
Sedrick Penate <digitalphenom0@gmail.com>
2023-04-29 21:10:15 -0700

    two exercises complete

---
+++ small_problems/debugging/reverse_sentence.rb
@@ -0,0 +1,18 @@
+
+def reverse_sentence(sentence)
+  words = sentence.split(' ')
+  reversed_words = []
+
+  i = 0
+  while i < words.length
+    reversed_words = reversed_words + [words[i]]
+    i += 1
+  end
+
+  reversed_words.join(' ')
+end
+
+# The method raised a typeError because line 8 tried to concatenate a String and Array, which you cant do. The solution is to wrap words[i] in brackets.
+
+p reverse_sentence('how are you doing')
+# output: 'doing you are how'
---
+++ small_problems/debugging/valid_series.rb
@@ -0,0 +1,18 @@
+
+def valid_series?(nums)
+  return false if nums.sum != 47
+
+  odd_count = nums.count { |n| n.odd? }
+  odd_count == 3 ? true : false
+end
+
+# the correct answer is to have the condition evaluate as true or false if it is equal to 3, then evaluate the ternary expression based on true or false. is 1 == 3 = false
+# false ? true : false
+
+
+
+p valid_series?([5, 6, 2, 7, 3, 12, 4, 8])        # should return true
+p valid_series?([1, 12, 2, 5, 16, 6])             # should return false
+p valid_series?([28, 3, 4, 7, 9, 14])             # should return false
+p valid_series?([20, 6, 9, 4, 2, 1, 2, 3])        # should return true
+p valid_series?([10, 6, 19, 2, 6, 4])             # should return false
