58d1d016018b0944a174c628bec02d5d66d0ea6f
digitalphenom <digitalphenom0@gmail.com>
2023-05-14 22:32:43 -0700

    updated answr

--- small_problems/easy_02/string_assignment.rb
+++ small_problems/easy_02/string_assignment.rb
@@ -6,23 +6,11 @@ save_name = name
 name = 'Alice'
 puts name, save_name
 
-
-# I dont remember comming accorss printing 2 variables at the same time.
-
-# Line 4 assigns the string object "Bob" to the variable (name)
-# Line 5, name is assigned a new variable (save_name) name & save_name now both point to the object "Bob"(pass by reference)
-# Line 6 takes a new String object "Alice" and assigns it to the variable (name).
-# because the variables (name) and (save_name) point to the same object 
-# line 7 will print  => "Alice", Alice
-
-#-------------------------------------------------------------------------------
-# I got this one wrong. remember that ruby is both pass by ref and pass by value depending on whether the objects are being mutated. in the example above theyre being re-assigned thus a new copy of each object is created for both name and save_name.
+# line 7 will output "Alice" and "Bob" respectively because re-assignment on line 6 caused `name` to point to the new object "Alice", this expression had no effect on `save_name` which still points to "Alice".
 
 name = 'Bob'
 save_name = name
 name.upcase!
 puts name, save_name
 
-# if you take this example on the other hand, your solution would apply here.
-# notice that theres transformation going on on line 24. This changes the way ruby outputs from pass by value to pass by reference
-# line 25 prints => BOB BOB
+# in this case the output is BOB and BOB because both name and save_name initially reference "Bob", when the mutating upcase! method is called, it modifies the string in place, and unlike the previous example both local variables still point to the initial object "Bob
