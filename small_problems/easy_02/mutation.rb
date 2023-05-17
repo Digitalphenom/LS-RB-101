1dd5f38bb3c9ff194e61717f0c5f6fb9fabff506
digitalphenom <digitalphenom0@gmail.com>
2023-05-17 16:10:28 -0700

    refactored line be line analysis

--- small_problems/easy_02/mutation.rb
+++ small_problems/easy_02/mutation.rb
@@ -5,15 +5,23 @@ array1.each { |value| array2 << value }
 array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
 puts array2
 
-# line 2 initializes a variable named (array1) and assigns it an array object.
-# line 3 initializes a variable named (array2) and assigns it an empty array.
-# line 4 calls the each method on (array1) with a block argument named value, within the block #each iterates over each value and appends each value into the empty (array2)
-# line 5 calls the #each method on (array1) and iterates over each value
-# upon each pass, it calls destructive #upcase! and uppercases letters that begin with the letters "C", and "S"
 
-# what will array2 print?
-# we know that line 4 destructively appends each value from array1 to array 2
-# but do they point to the same variable?
-# if we go based on the idea that destructive methods modify rubys output from pass by value to pass by refence, then line 4 should have us look for pass by reference.
-# therefore line 5 modifies both array1 and array2
-# line 6 should print [Moe Larry CURLY SHEMP Harpo CHICO Groucho Zeppo]
+#what will array2 print? Why is that the case?
+#  we know that line 4 destructively appends each value from array1 to array 2 #but do they point to the same variable?
+#  
+#  In this program, the call to puts with array2 as an argument will output:
+#  
+#  ```ruby
+#  Moe
+#  Larry
+#  CURRY
+#  SHEMP
+#  Harpo
+#  CHICO
+#  Grouco
+#  Zeppo
+#  => nil
+#  ```
+#  This is because on the first call to `each` on line 4, within the block the #expression `array2 << value` appends each element from array1 to array2. At #this point, both array1 and array2 reference the same elements and are #upcased with `value.upcase!` if the condition `value.start_with("C", "S")` #evaluates to `true`, when the program reaches the second call to `each` on #line 5.
+#  
+#  The concept at play is variables as pointers. When you use the shovel #operator `<<` to append an element from one array to another, youre actually #appending a reference to the original object, not creating a new one.
