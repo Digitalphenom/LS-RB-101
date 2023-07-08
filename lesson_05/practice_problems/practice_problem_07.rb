489db8a2c488ccad3e402b7368ca0dd7104bba78
digitalphenom <digitalphenom0@gmail.com>
2023-07-08 11:36:10 -0700

    problem revision

--- lesson_05/practice_problems/practice_problem_07.rb
+++ lesson_05/practice_problems/practice_problem_07.rb
@@ -5,5 +5,5 @@ arr = [a, b]
 arr[0] += 2
 arr[1][0] -= a
 
-# a = 4
-# b = [4, 8]
+
+# initially both the arr and a referenced the integer 2, after 2 was incremented by 2 a new object was assigned to the array which caused the local variable arr to no longer contain a reference to the integer 2. This means that `a` still reference 2. so when we decrement the final value in the nested array, we are decrementing by 2. # [4, [3, 8]]
--- lesson_05/practice_problems/practice_problem_08.rb
+++ lesson_05/practice_problems/practice_problem_08.rb
@@ -1,10 +1,21 @@
+# Using the each method, write some code to output all of the vowels from the strings.
+
 hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
 
-hsh.each_value do |value|
-  value.each do |word|
-    arr = word.chars
-      arr.each do |vowels|
-        vowels.match(/[aeiou]/) { |matchdata| puts matchdata }
+# you can also directly access the inner array along with the desired values and convert them into individual chars then iterate through each and perform the desired output.
+
+hsh.each do |_, arr|
+   arr[0..-1].join(" ").chars.each { |char| puts char if char =~ /[aeiou]/}
+end
+
+# -----------------------------------------------------
+
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
+
+hsh.each_value do |array|
+  array.each do |word|
+      word.chars.each do |char|
+        puts char if char =~ /[aeiou]/
       end
     end
 end
