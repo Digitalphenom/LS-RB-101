1504ab27bd98c6cf1d3493c9c9092e90664e0045
digitalphenom <digitalphenom0@gmail.com>
2023-07-13 17:56:16 -0700

    refactored generate_cards method to draw_card & split_deck

--- lesson_06/21_game.rb
+++ lesson_06/21_game.rb
@@ -14,40 +14,38 @@ def prompt(str)
   puts "=> #{str}"
 end
 
-def generate_hsh(cards)
-  arr = [{}, {}]
-  2.times do |i|
-    cards.each do |value|
-      arr.each do |hsh|
-      if value.is_a?(Array)
-        hsh[i] = cards.sample.sample
-      else
-        hsh[i] = cards.sample
+def draw_card(face, suit, count)
+  arr = []
+  count.times { arr << [suit.sample, face.sample.sample] }
+  arr
+end
+
+def split_deck(arr)
+  count = 0
+  deck = []
+  loop do
+    deck << arr[count..count + 1]
+    count += 2
+    break if count == arr.size
+  end
+  deck
+end
+
+def convert_face_to_digit(deck)
+  arr = 
+  deck.map do |sub_array|
+    sub_array.map do |key, value|
+      if %w(J Q K).include?(value)
+        10
+      elsif value != "A"
+        value.to_i
       end
-    end
     end
   end
   arr
 end
 
-  def convert_face_to_digit(deck)
-    arr = 
-    deck.map do |sub_array|
-      sub_array.map do |key, value|
-        if %w(J Q K).include?(value)
-          10
-        elsif value != "A"
-          value.to_i
-        end
-      end
-    end
-    arr
-  end
-
-
-  
   def display_deck(ps, pf, ds, df)
-    binding.pry
     system "clear"
     prompt "You're Bottom. Dealer is Top"
     
@@ -97,16 +95,16 @@ def player_turn
   # Start Game
   prompt "Welcome to the game 21!"
   prompt "Ready? Press (y) to Start"
-  start = gets.chomp
+  #start = gets.chomp
 
     #---------------------------------------------------------------
   arr_suit = SUITS.keys
   arr_face = SUITS.values
 
-  dealer_suit_hsh, user_suit_hsh = generate_hsh(arr_suit)
-  dealer_face_hsh, user_face_hsh = generate_hsh(arr_face)
-
-  dealer_face_deck, user_face_deck = convert_face_to_digit([dealer_face_hsh, user_face_hsh])
+  arr = draw_card(arr_face, arr_suit, 4)
+  dealer_cards, player_cards = split_deck(arr)
+  dealer_face_deck, user_face_deck = convert_face_to_digit([dealer_cards, player_cards])
+  binding.pry
   
   display_deck(user_suit_hsh, user_face_hsh, dealer_suit_hsh, dealer_face_hsh,)
   #binding.pry
