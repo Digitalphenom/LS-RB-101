f02e4c3e25b0e1a7ea4cdb8f682aff49e9a7b5df
digitalphenom <digitalphenom0@gmail.com>
2023-08-01 19:06:40 -0700

    refactored answer

---
+++ small_problems/code_wars/alphabet_symmetry.rb
@@ -0,0 +1,4 @@
+# Given an array of letters, return a number that represents how many letters occupy their proper index position in the alphabet.
+
+
+"sed"
--- small_problems/easy_07/capitalize_words.rb
+++ small_problems/easy_07/capitalize_words.rb
@@ -1,24 +1,30 @@
-def letter_map
-  letter_map = Hash.new
-  ("a".."z").each do |letter|
-    key = letter
-    value = (letter.ord - 32).chr
-    letter_map[key] = value
-  end
-  letter_map  
-end
+=begin
+Write a method that takes a single String argument and returns a new string that contains the original value of the argument with the first character of every word capitalized and all other letters lowercase.
+
+You may assume that words are any sequence of non-blank characters.
+
+Explicit Requirements
+- input: String
+- output: String
+- convert first character of each word to its uppercase equivalent and the rest of chars should be dowcased
+- words are any sequence of non-blank characters meaning whitespaces are skipped
+
+- What if there are non-alphateic type chars? you cant upcase a non alphabetic char so they must be skipped
+
+Data Structures
+If we utitlize an array we would need to first convert strings into an array of words => ["four", "score", "and", "seven"] then convert it back to a string
+
+Algorithm
+# seperate string by white space chars and convert to an array of substrings
+# access each word and convert the first char to its uppercase equivalent
+# convert array back into a string
+
+=end
 
 def word_cap(str)
-  map = letter_map()
-  arr = str.split
-  arr.each do |word|
-    if map.keys.include?(word[0])
-      word[0] = map[word[0]]
-    end
-  end
-  puts arr.join(" ")
-end
+  str.split.map { |word| word.capitalize }.join(" ")
+en
 
-word_cap('four score and seven') == 'Four Score And Seven'
-word_cap('the javaScript language') == 'The Javascript Language'
-word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
+p word_cap('four score and seven') == 'Four Score And Seven'
+p word_cap('the javaScript language') == 'The Javascript Language'
+p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
