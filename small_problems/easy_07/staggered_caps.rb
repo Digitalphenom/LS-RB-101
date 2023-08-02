fe5c55c867f9d5375890ab33c8d8d0baa0946faf
digitalphenom <digitalphenom0@gmail.com>
2023-08-02 13:47:37 -0700

    refactored staggered caps exercises

--- small_problems/easy_07/staggered_caps.rb
+++ small_problems/easy_07/staggered_caps.rb
@@ -1,17 +1,39 @@
-def staggered_case(str)
-  str = str.downcase
-  arr = str.chars
-  arr.each.with_index(1) do |char, i|
-    char.upcase! if i.odd?
-  end
-  arr.join
+
+# Write a method that takes a String as an argument, and returns a new String that contains the original value using a staggered capitalization scheme in which every other character is capitalized, and the remaining characters are lowercase. Characters that are not letters should not be changed, but count as characters when switching between upper and lowercase.
+
+# Explicit Requirements
+# Input: String
+# Output: String
+# Every other char should be converted to its uppercase equivalent
+# non-alphabetic chars should count as chars when switching between upper & lower case
+# Index position begins at 0 with first char capitalized
+# A new string should be returned
+
+# Implicit Requirements
+# whitespaces count as chars
+# digits do not count as chars 
+
+# Test Cases
+# - 'I LoVe lAuNcH ScHoOl!'
+# - 'AlL_CaPs' 0_1_0x1_0_1_0_1
+# 'IgNoRe 77 ThE 444 NuMbErS'
+
+# Access each individual char and determine its index position
+# keep track of whether the previous char was either a alphabetic char non non-alphabetic char
+# set a variable as nil
+# if it is an alphabetic char set variable as true & uppercase 
+# if it is not an alphabetic char set variable to false & downcase
+# if integers next
+
+# Data Structure
+# each incoming string will contain upper & lower case chars in no particular order
+
+def staggered_case(str, ii)
+  str.chars.map.with_index(ii) do |char, ii|
+    (ii.even? || ii.zero?) ? char.upcase : char.downcase
+  end.join
 end
 
-staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
-staggered_case('ALL_CAPS') == 'AlL_CaPs'
-staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
-# take a string
-# return a new string with every other character capitalized in staggered form
-# => "HeLlO WoRlD"
-
-# take a string and set it to all lowercase, then split it into single characters. iterate and upcase every other character in collection, if a character is not a word leave as is and go to next.
+p staggered_case('I Love Launch School!', 0) #== 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS', 0) #== 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers', 1) #== 'IgNoRe 77 ThE 444 NuMbErS'
--- small_problems/easy_07/staggered_caps_two.rb
+++ small_problems/easy_07/staggered_caps_two.rb
@@ -1,119 +1,21 @@
 
-# my original solutions long winded. heres a simpler approach
+# this approach manually keeps track of a counter so that if it encounters non-alphabetic chars they do not count as uppcase or downcase and the count continues from the previous alphabetic char
 
-def staggered_case(str)
-  flag = true
-  count = 0
-  result = ""
-
-  loop do
-    if flag == true && str[count].match?(/\s+|\d+/) == false
-      result += str[count].upcase
-      flag = false
-    elsif flag == false && str[count].match?(/\s+|\d+/) == false
-      result += str[count].downcase
-      flag = true
+def staggered_case(str, ii)
+  counter = ii
+  str.chars.map.with_index do |char|
+    if /[\W \d]/.match?(char)
+      char
+    elsif counter.even? || counter.zero? 
+      counter += 1
+      char.upcase
     else
-      result += str[count]
+      counter += 1
+      char.downcase
     end
-    count += 1
-    break if count == str.size
-  end
-  result
+  end.join
 end
 
-# this can be further refactored to:
-
-def staggered_case(str)
-  flag = true
-  result = ""
-
-  str.chars.each do |char|
-    flag ? result += char.upcase : result += char.downcase
-
-    next if char.match?(/[^a-zA-Z]/)
-    flag = !flag
-  end
-  result
-end
-
-# +++++++++++++++++++++++(original solution)+++++++++++++++++++++
-# places digits in a number vault, saves the space and digit index then reinserts them after alphanumeric chars are staggered.
-
-def space_index(str)
-  space_index = []
-  arr = str.chars
-
-  arr.each.with_index do |char, i|
-    space_index << i if char == " " 
-  end
-  space_index
-end
-
-def digit_index(str)
-  digit_index = []
-  str = str.downcase.split.join
-  arr = str.chars
-
-  arr.each.with_index do |char, i|
-    digit_index << i if char.match?(/\d/)
-  end
-  digit_index
-end
-
-def number_vault(str)
-  number_vault = []
-  arr = str.chars
-  
-  arr.each do |char|
-    number_vault << char if char.match?(/\d/)
-  end
-  number_vault
-end
-
-def staggered_case_two(str)
-  space_index = space_index(str)
-  digit_index = digit_index(str)
-  numbers = number_vault(str)
-
-  str.gsub!(/\d/, "")
-  str = str.downcase.split.join
-
-  arr = str.chars
-  arr.each.with_index(1) do |char, i|
-    char.upcase! if i.odd?
-  end
-  str = arr.join
-  # re-insert digits
-  count = 0
-  until count == numbers.length
-       str.insert(digit_index[count], numbers[count])
-       count += 1
-  end
- # re-insert spaces
-   space_index.each do |i|
-    str.insert(i, " ")
-  end
-  str
-end
-
- staggered_case_two('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
- staggered_case_two('ALL CAPS') == 'AlL cApS'
- staggered_case_two('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
-
-
-#p staggered_case_two('ALL CAPS') == 'AlL cApS'
-#p staggered_case_two('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
-
-# take a string
-# return a new string with every other character capitalized in staggered form
-# => "HeLlO WoRlD"
-
-# take a string and set it to all lowercase, then split it into single characters. iterate and upcase every other character in collection, if a character is not a word leave as is and go to next.
-
-# the most direct to accomplish this is by downcasing then joining all arrays excluding spaces.
-# the problem is how to insert spaces back into the joined.
-# one way to solve this is to iterate through the initial string and save the index to an array
-# then iterate through characters inserting an empty space into the proper index
-
-# [s,e,d, r,i,c,k]
+p staggered_case('I Love Launch School!', 0) == 'I lOvE lAuNcH sChOoL!'
+p staggered_case('ALL CAPS', 0) == 'AlL cApS'
+p staggered_case('ignore 77 the 444 numbers', 0) == 'IgNoRe 77 ThE 444 nUmBeRs'
