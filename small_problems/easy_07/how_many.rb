7656f4bd67cfbadbc58c9a1e513294bdecfdf91f
digitalphenom <digitalphenom0@gmail.com>
2023-08-02 19:44:17 -0700

    added pedac

--- small_problems/easy_07/how_many.rb
+++ small_problems/easy_07/how_many.rb
@@ -1,3 +1,30 @@
+# Problem
+# Write a method that counts the number of occurrences of each element in a given array
+
+# Explicit Requirements
+# Input: array
+# Output: Hash
+# count total occurences of each vehicle type
+
+# Implicit Requirements
+# pair total occurences with vehicle type
+
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
+
+# Data Strucutre
+# {vehicle_type => total_occurences}
+
+# Algorithm
+# set an empty hash
+# access each vehicle type in array
+# insert each unique vehicle type into hash
+#   - insert current vehicle into hash key and increment value to 1
+# hash[type] = 0 + 1
+# return hash
+
 def count_occurrences(vehicles)
   hash = Hash.new(0)
   vehicles.each { |type| hash[type] += 1 }
