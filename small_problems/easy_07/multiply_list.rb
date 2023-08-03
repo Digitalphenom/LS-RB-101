858563ecdb8369e5d6c272dab9b676dadc6047de
digitalphenom <digitalphenom0@gmail.com>
2023-08-02 18:20:24 -0700

    added pedac

--- small_problems/easy_07/multiply_list.rb
+++ small_problems/easy_07/multiply_list.rb
@@ -1,3 +1,27 @@
+# P:
+# Write a method that takes two Array arguments in which each Array contains a list of numbers, and returns a new Array that contains the product of each pair of numbers from the arguments that have the same index. You may assume that the arguments contain the same number of elements.
+
+# Explicit Requirements
+# Input: Array
+# Output: Array
+# Return new array
+# must multiply integers in the same index position
+# array arguments will always be equal in size 
+
+# Notes
+# the product is a value that is the result of multiply two or more digits
+
+# E:
+# [3, 5, 7], [9, 10, 11] => [27, 50, 77]
+# arr1_index_position 0 * arr2_index_position 0
+
+# Algorithm
+# create a new arra where we insert each product
+# access value at index position 0 for both arr1 & arr2 then multiply
+#   new_array << arr[i] * arr2[i]
+# append product to new array
+# continue upto the size of array
+
 def multiply_list(array1, array2)
   array1.map.with_index do |num, i|
      num * array2[i]
