1f3615772ec5bd9a99adc33a6cd1eb3bf5a30cdd
digitalphenom <digitalphenom0@gmail.com>
2023-09-02 16:01:33 -0700

    updated answer

--- small_problems/medium_02/lettercase_ratio.rb
+++ small_problems/medium_02/lettercase_ratio.rb
@@ -1,30 +1,69 @@
 
-def letter_count(str)
-  case_count = {:lowercase => 0, :uppercase => 0, :neither => 0}
-  str.each_char do |chr|
-    if chr.match?(/[a-z]/)
-      case_count[:lowercase] += 1
-    elsif chr.match?(/[A-Z]/)
-      case_count[:uppercase] += 1
+=begin
+◸≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂◟ 𝓟roblem ◞≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂◹
+
+Write a method that takes a string, and then returns a hash that contains 3 entries: one represents the percentage of characters in the string that are lowercase letters, one the percentage of characters that are uppercase letters, and one the percentage of characters that are neither.
+
+Given an input string, return a hash with three entries:
+{lowercase => percentage}
+{uppercase => percentage}
+{neither => percentage}
+
+You may assume that the string will always contain at least one character.
+
+● Implicity Requirements
+  ⚬ include empty space chars in neither
+
+◸≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂◟ Examples ◞≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂◹
+
+abCdef 123'
+{lowercase => 4 } ?
+{lowercase => 4 } ?
+{lowercase => 4 } ?
+
+◸≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂◟ Algorithm ◞≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂≂◹
+
+? How to expess a total count as a percentage of total
+  ● Divide the whole by 100 & multiply by the part
+
+? How to count each category of chars?
+  ● set 3 counts
+    ⚬ upcase, downcase, neither
+  ● Access each char and add one to the corresponding count when a given char is encountered
+
+? How to insert percentage as a hash value in a corresponding key
+  ● access each count, and assign as a hash value
+
+=end
+
+GROUPS = [:lowercase, :uppercase, :neither]
+
+def add_count(str)
+  lowercase = 0; uppercase = 0; neither = 0
+
+  str.each_char do |char|
+    if char.match?(/[a-z]/)
+      lowercase += 1
+    elsif char.match?(/[A-Z]/)
+      uppercase += 1
     else
-      case_count[:neither] += 1
+      neither += 1
     end
   end
-  case_count
+  [lowercase, uppercase, neither]
 end
 
 def letter_percentages(str)
-  length = str.chars.size
+  cases = add_count(str)
 
-  case_count = letter_count(str)
-  case_count[:lowercase] = (case_count[:lowercase] / (length.to_f)).ceil(3) * 100
-  case_count[:uppercase] = (case_count[:uppercase] / (length.to_f)).ceil(3) * 100
-  case_count[:neither]  = (case_count[:neither] / (length.to_f)).ceil(3) * 100
-  case_count
+  hsh = cases.each_with_object({}).with_index do |(value, hsh), i|
+    hsh[GROUPS[i]] = (100 / str.size.to_f ) * value
+  end
 end
 
-p letter_percentages('abcdefGHI') 
-p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
-p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
+
+p letter_percentages('abCdef 123') # == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
+p letter_percentages('AbCd +Ef') # == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
+p letter_percentages('123') # == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
 
 
