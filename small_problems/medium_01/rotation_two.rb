99392211d93bee9cf9f8086b9534ba7861c03973
digitalphenom <digitalphenom0@gmail.com>
2023-09-03 14:35:17 -0700

    refined answer

--- small_problems/medium_01/rotation_two.rb
+++ small_problems/medium_01/rotation_two.rb
@@ -1,20 +1,17 @@
-def rotate_array(array, n)
-  temp = array.dup
-  temp << temp.slice!(n)
+def rotate_array(arr)
+  arr[1..-1] + [arr[0]]
 end
 
-def rotate_rightmost_digits(digits, n)
-  arr = digits.digits.reverse
-  rotate = rotate_array(arr, -n)
-
-  result = 0
-  rotate.each { |num| result = result * 10 + num }
-  result
+def rotate_rightmost_digits(nums, n)
+ digit_arr = nums.digits.reverse
+ sliced = digit_arr[-n..-1] #  0...-n..-1
+ 
+ (digit_arr[0...-n] + rotate_array(sliced)).join.to_i #  0...-n..-1
 end
 
-rotate_rightmost_digits(735291, 1) == 735291
-rotate_rightmost_digits(735291, 2) == 735219
-rotate_rightmost_digits(735291, 3) == 735912
-rotate_rightmost_digits(735291, 4) == 732915
-rotate_rightmost_digits(735291, 5) == 752913
-rotate_rightmost_digits(735291, 6) == 352917
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
